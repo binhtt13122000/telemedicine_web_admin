ce426d8482301aea18b180ef3d65b2aa57e62b10
nhanlt1602 <nhanlt16235@gmail.com>
2021-09-26 20:38:37 +0700

    [26/09-NhanLT] change address of snackbar success in app.tsx

---
+++ src/Snackbar/SnackbarError/index.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from "react";
+
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import Stack from "@mui/material/Stack";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const SnackbarError: React.FC = (): ReactElement => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+        return "test";
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        return "test";
+    };
+
+    return (
+        <Stack spacing={2} sx={{ width: "10%" }}>
+            <Button variant="outlined" onClick={handleClick}>
+                Error Snackbar
+            </Button>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+                    Error
+                </Alert>
+            </Snackbar>
+        </Stack>
+    );
+};
+
+export default SnackbarError;
---
+++ src/container/SymptomPage/index.tsx
@@ -0,0 +1,66 @@
+import React, { ReactElement, useEffect, useState } from "react";
+
+import axios from "axios";
+import MaterialTable from "material-table";
+
+const SymptomPage: React.FC = (): ReactElement => {
+    const columns = [
+        { title: "ID", field: "id" },
+        { title: "Symptom code", field: "symptomCode" },
+        { title: "Name", field: "name" },
+        { title: "Description", field: "description" },
+    ];
+
+    const [datatable, setDatatable] = useState([]);
+    const [currentPage, setCurrentPage] = useState();
+    const [totalItem, setTotalItem] = useState(0);
+
+    useEffect(() => {
+        const axiosGet = async () => {
+            const response = await axios(
+                "http://52.221.193.237/api/v1/symptoms?code=1&limit=20&offset=1"
+            );
+            // console.log(response.data.content);
+            // console.log(response.data.currentPage);
+            setDatatable(response.data.content);
+            setCurrentPage(response.data.currentPage);
+            setTotalItem(response.data.totalCount);
+        };
+        axiosGet();
+    }, []);
+
+    return (
+        <div>
+            <MaterialTable
+                title="Manage Symptom"
+                columns={columns}
+                data={datatable}
+                editable={{
+                    onRowAdd: (newData) =>
+                        new Promise((resolve) => {
+                            setTimeout(() => {
+                                /* setData([...data, newData]); */
+                                setDatatable(newData);
+                                resolve;
+                            }, 1000);
+                        }),
+                }}
+                actions={[
+                    () => ({
+                        icon: "delete",
+                        tooltip: "Delete User",
+                        onClick: (event, rowData) => alert("You saved " + rowData),
+                    }),
+                ]}
+                options={{
+                    actionsColumnIndex: -1,
+                    addRowPosition: "first",
+                }}
+                totalCount={totalItem}
+                page={currentPage}
+            />
+        </div>
+    );
+};
+
+export default SymptomPage;
